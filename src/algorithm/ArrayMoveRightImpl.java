8a6b0b500a1291c9b4d9ea0bddc956a65ba29292
xuye <1554922833@qq.com>
2018-01-23 19:18:29 +0800

    思考将数组指定元素提前的算法

--- src/algorithm/ArrayMoveRightImpl.java
+++ src/algorithm/ArrayMoveRightImpl.java
@@ -1,5 +1,6 @@
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -92,11 +93,96 @@ public class ArrayMoveRightImpl {
 		}
 	}
 
+	/**
+	 * 从数组中取出指定值，将所有指定值都提到数组前面，其他的非指定值保持顺序后移，要求空间复杂度尽量小<br/>
+	 * 例子：[a0b0c]变成[00abc]<br/>
+	 * 
+	 * @param array
+	 *            待操作的数组
+	 * @param k
+	 *            待移动的指定值
+	 * @return 移动后的数组
+	 */
+	public static char[] moveValueToFrontSimple(char[] array, char value) {
+		// 简单的思路就是从数组中取出所有指定值，再取出所有非指定值，然后在指定值数组后添加非指定值
+		ArrayList<Character> valueList = new ArrayList<Character>();// 存放指定value
+		ArrayList<Character> otherList = new ArrayList<Character>();// 存放其他value
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				valueList.add(array[i]);
+			} else {
+				otherList.add(array[i]);
+			}
+		}
+		valueList.addAll(otherList);
+		for (int j = 0; j < array.length; j++) {
+			array[j] = valueList.get(j);
+		}
+		return array;
+	}
+
+	/**
+	 * 这种方法不需要额外数组，只需要个额外变量保存待交换的值，空间复杂度应该是(1)，时间复杂度没算出来。。。
+	 */
+	public static char[] moveValueToFrontBetter(char[] array, char value) {
+		// 从头开始遍历，如果不是指定值，则在其后面范围找指定值，找到交换，并将原本的值插入到查找范围里，保证原顺序
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] != value) {
+				System.out.println("发生了交换查找：i=" + i + ", array[i]=" + array[i]);
+				for (int j = i + 1; j < array.length - 1; j++) {
+					if (array[j] == value) {
+						System.out.println("交换查找后遍历：j=" + j + ", array[j]="
+								+ array[j]);
+						char temp = array[i];// 保存一下要交换的非指定值
+
+						// 后移查找范围所有值，并把非指定值插入到查找范围的首位
+						int beginIndex = i + 1;
+						int endIndex = j;
+						insertValueInStart(array, beginIndex, endIndex, temp);
+
+						// 将指定值赋值给待交换的位置
+						array[i] = value;
+						System.out.println("插入并交换后，数组值为："
+								+ Arrays.toString(array));
+						break;
+					}
+				}
+
+				// 优化时间复杂度
+				if (array[i] != value) {
+					System.out.println("本次交换查找没有找到指定值，后续无需再遍历，程序结束");
+					// 遍历结束后，也没找到指定值(即没发生过交换，array[i]不是value)
+					return array;
+				}
+			}
+		}
+		return array;
+	}
+
+	/**
+	 * 将value插入指定范围最前面，然后将范围内的值全后移1位
+	 */
+	public static void insertValueInStart(char[] array, int beginIndex,
+			int endIndex, char insertValue) {
+		for (int i = endIndex; i > beginIndex; i--) {
+			// 右移所有元素，最后1位被挤掉
+			array[i] = array[i - 1];
+		}
+		// 将要插入的值，赋值给begin，完成插入
+		array[beginIndex] = insertValue;
+	}
+
 	public static void main(String[] args) {
-		char[] array = new char[] { '1', '0', 'a', '0', 'c' };
+		char[] array = new char[] { 'a', 'b', '0', '0', 'c' };
 		System.out.println("右移前的数组：" + Arrays.toString(array));
 		// arrayMoveRightSimple(array, 2);
 		arrayMoveRightBetter(array, 2);
 		System.out.println("右移后的数组：" + Arrays.toString(array));
+
+		array = new char[] { 'a', 'b', '0', '0', 'c' };
+		System.out.println("\n----元素提取前的数组：" + Arrays.toString(array) + "\n");
+		// moveValueToFrontSimple(array, '0');
+		moveValueToFrontBetter(array, '0');
+		System.out.println("\n----元素提取后的数组：" + Arrays.toString(array));
 	}
 }
